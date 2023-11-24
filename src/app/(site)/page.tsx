e9a4ec0241cd38bc296d4735468d9cddb67bddcc
Ian Chege <ianchege001@gmail.com>
2023-11-24 17:07:46 +0300

    feat: setting up ORM and landing-page

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,13 @@
+import TitleSection from '@/components/landing-page/title-section'
+
+const HomePage = () => {
+  return (
+    <section>
+      <div className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
+        <TitleSection title="All-in-One Collaboration and Productivity Platform" pill="✨ Your Workspace, Perfected" />
+      </div>
+    </section>
+  )
+}
+
+export default HomePage
