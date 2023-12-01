d1855514917d8342a3b2db1d852026f09a415e6f
Ian Chege <en20101832020@student.mut.ac.ke>
2023-12-01 18:30:08 +0300

    feat: implement login and signup pages

--- src/components/landing-page/title-section.tsx
+++ src/components/landing-page/title-section.tsx
@@ -17,7 +17,7 @@ const TitleSection: React.FC<TitleSectionProps> = ({ title, subheading, pill }) 
             <p className="dark:text-washed-purple-700 sm:max-w-[450px] md:text-center">{subheading}</p>
           </>
         ) : (
-          <h1 className="text-left text-4xl sm:text-6xl sm:max-w-[850px] md: text-center font-semibold">{title}</h1>
+          <h1 className="text-left text-4xl sm:text-6xl sm:max-w-[850px] md:text-center font-semibold">{title}</h1>
         )}
       </section>
     </>
